0e047917bd0a4608f40a6fd13f4b374e8c43f128
Ray <huangjuite@gmail.com>
2019-04-20 00:57:31 +0800

    update

--- matlab/system_id.m
+++ matlab/system_id.m
@@ -2,7 +2,7 @@ clear;clc;
 data = load("data5.mat");
 data = data.data;
 phi = data(:,1)/180*pi;
-range=260:295;
+range=261:295;
 
 %smoothing phi
 for i=2:length(phi)-1
@@ -15,10 +15,6 @@ for i=2:length(phi)
     phid(i-1) = (phi(i)-phi(i-1))/dt;
 end
 phid(length(phi)) = phid(end);
-%smoothing phid
-for i=2:length(phid)-1
-    phid(i) = (phid(i+1)+phid(i-1))/2;
-end
 phid = phid';
 
 %get phidd
@@ -26,10 +22,6 @@ for i=2:length(phid)
     phidd(i-1) = (phid(i)-phid(i-1))/dt;
 end
 phidd(length(phid)) = phidd(end);
-%smoothing phidd
-for i=2:length(phidd)-1
-    phidd(i) = (phidd(i+1)+phidd(i-1))/2;
-end
 phidd = phidd';
 
 g2 = -2*cos(phi(range)).*phidd(range)+sin(2*phi(range)).*sec(phi(range)).*phid(range).*phid(range);
@@ -51,50 +43,30 @@ dt = 0.01;
 
 %get thetad
 thetad = data2(:,2)/180*pi;
-%smoothing thetad
-for i=2:length(thetad)-1
-    thetad(i) = (thetad(i+1)+thetad(i-1))/2;
-end
 
 %get thetadd
 for i=2:length(thetad)
     thetadd(i-1) = (thetad(i)-thetad(i-1))/dt;
 end
 thetadd(length(thetad)) = thetadd(end);
-%smoothing thetadd
-for i=2:length(thetadd)-1
-    thetadd(i) = (thetadd(i+1)+thetadd(i-1))/2;
-end
 thetadd = thetadd';
 
 %get phi
 phi = data2(:,1)/180*pi;
-%smoothing phi
-for i=2:length(phi)-1
-    phi(i) = (phi(i+1)+phi(i-1))/2;
-end
+
 %get phid
 dt = 0.01; 
 for i=2:length(phi)
     phid(i-1) = (phi(i)-phi(i-1))/dt;
 end
 phid(length(phi)) = phid(end);
-%smoothing phid
-for i=2:length(phid)-1
-    phid(i) = (phid(i+1)+phid(i-1))/2;
-end
+phid = phid';
 
 %get phidd
 for i=2:length(phid)
     phidd(i-1) = (phid(i)-phid(i-1))/dt;
 end
 phidd(length(phid)) = phidd(end);
-%smoothing phidd
-for i=2:length(phidd)-1
-    phidd(i) = (phidd(i+1)+phidd(i-1))/2;
-end
-
-phid = phid';
 phidd = phidd';
 
 u = zeros(length(phi),1);
@@ -121,5 +93,7 @@ G4 = [g6 g7 g10];
 A2 = pinv(G3'*G3)*G3'*E1;
 B2 = pinv(G4'*G4)*G4'*E2;
 
-a = [A2(1) A1(1) A2(2) A1(2) A2(3)];
-b = [B2(1) B2(2) B1(1) B1(2) B2(3)];
+a = [A2(1) A1(1) A2(2) A1(2) A2(3)]
+b = [B2(1) B2(2) B1(1) B1(2) B2(3)]
+
+
