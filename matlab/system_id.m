17ab37542e5f3cc9198c0bd30ec0d9abd2477734
Ray <huangjuite@gmail.com>
2019-04-19 21:38:00 +0800

    update system id

--- matlab/system_id.m
+++ matlab/system_id.m
@@ -2,35 +2,12 @@ clear;clc;
 data = load("data5.mat");
 data = data.data;
 phi = data(:,1)/180*pi;
-thetad = data(:,2);
-u = zeros(length(phi),1);
-for i=1:length(phi)
-   u(i)=0; 
+range=260:285;
+
+%smoothing phi
+for i=2:length(phi)-1
+    phi(i) = (phi(i+1)+phi(i-1))/2;
 end
-plot(phi),grid
-range=250:400;
-
-%get thetadd
-dt = 0.01;
-for i=2:length(thetad)
-    thetadd(i-1) = (thetad(i)-thetad(i-1))/dt;
-end
-thetadd(length(thetad)) = thetadd(end);
-thetadd = thetadd';
-
-
-
-%smoothing
-%{
-head = 130;
-tail = 160;
-A=[head*head head 1;tail*tail tail 1;(tail+1)*(tail+1) (tail+1) 1];
-coe = pinv(A)*[phi(head);phi(tail);phi(tail+1)];
-for i=head+1:tail-1
-    phi(i)=i*i*coe(1)+i*coe(2)+coe(3);
-end
-%}
-
 
 %get phid
 dt = 0.01; 
@@ -38,6 +15,10 @@ for i=2:length(phi)
     phid(i-1) = (phi(i)-phi(i-1))/dt;
 end
 phid(length(phi)) = phid(end);
+%smoothing phid
+for i=2:length(phid)-1
+    phid(i) = (phid(i+1)+phid(i-1))/2;
+end
 phid = phid';
 
 %get phidd
@@ -45,10 +26,83 @@ for i=2:length(phid)
     phidd(i-1) = (phid(i)-phid(i-1))/dt;
 end
 phidd(length(phid)) = phidd(end);
+%smoothing phidd
+for i=2:length(phidd)-1
+    phidd(i) = (phidd(i+1)+phidd(i-1))/2;
+end
 phidd = phidd';
 
-g1 = -thetadd(range);
 g2 = -2*cos(phi(range)).*phidd(range)+sin(2*phi(range)).*sec(phi(range)).*phid(range).*phid(range);
+g4 = phid(range);
+g8 = -phid(range);
+g9 = sin(phi(range));
+E = phidd(range);
+G1 = [g2 g4];
+G2 = [g8 g9];
+A1 = pinv(G1'*G1)*G1'*E;
+B1 = pinv(G2'*G2)*G2'*E;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+data2 = load("data7.mat");
+data2 = data2.data;
+range=85:99;
+
+u = zeros(length(phi),1);
+for i=1:length(phi)
+   u(i)=5; 
+end
+
+%get thetad
+thetad = data2(:,2)/180*pi;
+%smoothing thetad
+for i=2:length(thetad)-1
+    thetad(i) = (thetad(i+1)+thetad(i-1))/2;
+end
+
+%get thetadd
+dt = 0.01;
+for i=2:length(thetad)
+    thetadd(i-1) = (thetad(i)-thetad(i-1))/dt;
+end
+thetadd(length(thetad)) = thetadd(end);
+%smoothing thetadd
+for i=2:length(thetadd)-1
+    thetadd(i) = (thetadd(i+1)+thetadd(i-1))/2;
+end
+thetadd = thetadd';
+
+%get phi
+phi = data2(:,1)/180*pi;
+%smoothing phi
+for i=2:length(phi)-1
+    phi(i) = (phi(i+1)+phi(i-1))/2;
+end
+
+%get phid
+dt = 0.01; 
+for i=2:length(phi)
+    phid(i-1) = (phi(i)-phi(i-1))/dt;
+end
+phid(length(phi)) = phid(end);
+%smoothing phid
+for i=2:length(phid)-1
+    phid(i) = (phid(i+1)+phid(i-1))/2;
+end
+
+%get phidd
+for i=2:length(phid)
+    phidd(i-1) = (phid(i)-phid(i-1))/dt;
+end
+phidd(length(phid)) = phidd(end);
+%smoothing phidd
+for i=2:length(phidd)-1
+    phidd(i) = (phidd(i+1)+phidd(i-1))/2;
+end
+
+
+g1 = -thetadd(range);
+g2 = -2*cos(phi(range)).*phidd(range)+sin(2.*phi(range)).*sec(phi(range)).*phid(range).*phid(range);
 g3 = -thetad(range);
 g4 = phid(range);
 g5 = u(range);
@@ -57,8 +111,13 @@ g7 = -cos(phi(range)).*thetadd(range);
 g8 = thetad(range)-phid(range);
 g9 = sin(phi(range));
 g10 = -u(range);
-G1=[g1 g2 g3 g4 g5];
-G2=[g6 g7 g8 g9 g10];
-E = phidd(range);
-a = pinv(G1'*G1)*G1'*E
-b = pinv(G2'*G2)*G2'*E
+E1 = phidd(range) - A1(1).*g2 - A1(2).*g4;
+E2 = phidd(range) - B1(1).*g8 - B1(2).*g9;
+G3 = [g1 g3 g5];
+G4 = [g6 g7 g10];
+
+A2 = pinv(G3'*G3)*G3'*E1;
+B2 = pinv(G4'*G4)*G4'*E2;
+
+a = [A2(1) A1(1) A2(2) A1(2) A2(3)];
+b = [B2(1) B2(2) B1(1) B1(2) B2(3)];
