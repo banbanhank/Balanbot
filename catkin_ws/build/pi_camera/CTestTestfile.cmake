c13a0e1ba7dc4ce6f7376a59b5977bc4603bb6c9
ray <huangjuite@gmail.com>
2019-05-06 23:59:40 +0800

    update

--- catkin_ws/build/pi_camera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robotx/Balanbot/catkin_ws/src/pi_camera
-# Build directory: /home/robotx/Balanbot/catkin_ws/build/pi_camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
