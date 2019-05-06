03d2803c04f9de870a145722794226b9a985dcfe
ray <huangjuite@gmail.com>
2019-05-06 23:57:11 +0800

    add ros

---
+++ catkin_ws/build/pi_camera/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotx/Balanbot/catkin_ws/src/pi_camera
+# Build directory: /home/robotx/Balanbot/catkin_ws/build/pi_camera
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
